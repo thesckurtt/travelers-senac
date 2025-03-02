49ced4f1dfe693517977669744a6c497a7c66843
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-03-02 17:07:00 -0300

    add animation

---
+++ js/animation.js
@@ -0,0 +1,8 @@
+$('.div-btn-travel').on('click', function() {
+  $(this).attr('transition-style', 'in:wipe:right');
+
+  // Aguarda 500ms antes de remover o atributo
+  setTimeout(() => {
+    $(this).removeAttr('transition-style');
+  }, 500); // Ajuste o tempo conforme necessário
+});
